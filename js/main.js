effc07e0d86bd56495201c4280e9170db4d469d2
tajbiul-hossain <tbh.nishat@gmail.com>
2021-07-20 07:51:01 +0600

    second commit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,32 @@
 $(function () {
 
+    var $scroll;
+    $(window).scroll(function () {
+        $scroll = $(window).scrollTop();
+        if ($scroll > 100) {
+            $(".navbar").css("background-color", "rgba(20, 2, 0, 0.8)");
+        } else {
+            $(".navbar").css("background-color", "rgba(0, 0, 0, 0)");
+        }
+    })
+
     // Gets the video src from the data-src on each button
-    var $videoSrc;
+    var videoSrc;
     $(".video-btn").click(function () {
-        $videoSrc = $(this).data("src");
-    });
+        videoSrc = $(this).data("src");
+    })
 
     // when the modal is opened autoplay it  
     $("#myModal").on('shown.bs.modal', function (e) {
 
         // set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
-        $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        $("#video").attr('src', videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
     })
 
     // stop playing the youtube video when I close the modal
     $('#myModal').on('hide.bs.modal', function (e) {
         // a poor man's stop video
-        $("#video").attr('src', $videoSrc);
+        $("#video").attr('src', videoSrc);
     })
 
 
@@ -24,7 +34,7 @@ $(function () {
     $(".count").counterUp({
         delay: 10,
         time: 1000
-    });
+    })
 
 
     // document ready  
